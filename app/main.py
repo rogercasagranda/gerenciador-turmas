120bc4b1c383aef87907fa80dae507c71be379ad
rogercasagranda <roger.rodriguez05@gmail.com>
2025-07-31 21:47:53 -0300

    docs: clarify FastAPI instance comment

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
-app = FastAPI()  # <=== ESSA LINHA É OBRIGATÓRIA
-#TESTE 
+app = FastAPI()  # Instância da aplicação FastAPI
+
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
