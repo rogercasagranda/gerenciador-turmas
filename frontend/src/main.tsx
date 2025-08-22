9d8a34e94923255c6f3c3f81f98738a3efe46a66
rogercasagranda <roger.rodriguez05@gmail.com>
2025-08-21 21:35:37 -0300

    feat: centralize base styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 // frontend/src/main.tsx
 
 import './styles/theme.css';
+import './styles/global.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
